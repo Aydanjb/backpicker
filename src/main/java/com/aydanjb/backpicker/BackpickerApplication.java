1465afd94c976a0c28d5be1363631f4227aa9e08
Aydanjb <AydanBondurant@gmail.com>
2025-07-26 01:39:35 -0400

    initial commit

---
+++ src/main/java/com/aydanjb/backpicker/BackpickerApplication.java
@@ -0,0 +1,13 @@
+package com.aydanjb.backpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aydanjb/backpicker/BackpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aydanjb.backpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
